925201fa8bf179d66e4cfbcff3f59c3505126a66
Ibrahim Hadidy <ebrahemsaleh14@gmail.com>
2023-11-13 17:07:03 +0200

    Refactored the homeTabs code to be less longer and added missing css styles

--- src/pages/Store/HomeTabs/HomeTabs.tsx
+++ src/pages/Store/HomeTabs/HomeTabs.tsx
@@ -1,10 +1,5 @@
 import { FC, useEffect, useState } from "react";
-import {
-  newAndTrending,
-  topSellers,
-  popularUpcoming,
-  specials,
-} from "./homeTabsItems";
+import { newAndTrending, topSellers, popularUpcoming, specials, } from "./homeTabsItems";
 import "./HomeTabs.css";
 
 interface TabItem {
@@ -15,7 +10,7 @@ interface TabItem {
   image2?: string;
   image3?: string;
   image4?: string;
-  tag1?: string;
+  tag1?: string | number;
   tag2?: string;
   tag3?: string;
   tag4?: string;
@@ -35,18 +30,20 @@ const TabContent: FC<{
   onTabHover: (index: number | null) => void;
 }> = ({ items, title, isOpened, onTabHover }) => {
   const [focusedTab, setFocusedTab] = useState<number | null>(null);
+  const [hasHovered, setHasHovered] = useState(false);
+
+  useEffect(() => {
+    if (!hasHovered && isOpened) {
+      setFocusedTab(0);
+      onTabHover(0);
+      setHasHovered(true);
+    }
+  }, [title, isOpened, onTabHover, hasHovered]);
 
   const handleMouseEnter = (index: number) => {
     setFocusedTab(index);
     onTabHover(index);
   };
-
-  useEffect(() => {
-    if (title === "New & Trending") {
-      setFocusedTab(0);
-      onTabHover(0);
-    }
-  }, [title, onTabHover]);
 
   return (
     <div
@@ -78,12 +75,12 @@ const TabContent: FC<{
               <div className="tab-final-price">{tabItem.price}</div>
             ) : (
               <>
-                <div className="discount-percentage">
-                  {tabItem.discountPercentage}
-                </div>
                 <div className="discount-prices">
                   <div className="original-price">{tabItem.price}</div>
                   <div className="final-price">{tabItem.discountPrice}</div>
+                </div>
+                <div className="discount-percentage">
+                  {tabItem.discountPercentage}
                 </div>
               </>
             )}
@@ -95,9 +92,9 @@ const TabContent: FC<{
               <span className={tabItem.mac || ""}></span>
               <div className="tab-item-top-tags">
                 <span className="tab-top-tag">{tabItem.tag1}</span>
-                <span className="tab-top-tag">{tabItem.tag2}</span>
-                <span className="tab-top-tag">{tabItem.tag3}</span>
-                <span className="tab-top-tag">{tabItem.tag4}</span>
+                <span className="tab-top-tag">{(tabItem.tag2)&&", "}{tabItem.tag2}</span>
+                <span className="tab-top-tag">{(tabItem.tag3)&&", "}{tabItem.tag3}</span>
+                <span className="tab-top-tag">{(tabItem.tag4)&&", "}{tabItem.tag4}</span>
               </div>
             </div>
           </div>
@@ -107,8 +104,64 @@ const TabContent: FC<{
   );
 };
 
+const getImageURL = (
+  openedTab: string | number | never,
+  hoveredTabIndex: string | number,
+  imageNumber: number
+) => {
+  const tabs = {
+    "New & Trending": newAndTrending,
+    "Top Sellers": topSellers,
+    "Popular Upcoming": popularUpcoming,
+    "Specials": specials,
+  };
+  //@ts-expect-error "any type error"
+  const currentTab = tabs[openedTab] || [];
+  return currentTab[hoveredTabIndex]?.[`image${imageNumber}`] || "";
+};
+
+const renderScreenshots = (
+  openedTab: string | number,
+  hoveredTabIndex: number
+) => {
+  return [...Array(4).keys()].map((index) => (
+    <div
+      className="tabs-screenshot"
+      style={{
+        backgroundImage: `url(${getImageURL(
+          openedTab,
+          hoveredTabIndex,
+          index + 1
+        )})`,
+      }}
+      key={index}
+    ></div>
+  ));
+};
+
+interface TabProps {
+  tabName: string;
+  tabTitle: string;
+  handleTabClick: (tabTitle: string) => void;
+  openedTab: string | number;
+}
+
+const Tab: React.FC<TabProps> = ({
+  tabName,
+  tabTitle,
+  handleTabClick,
+  openedTab,
+}) => (
+  <div
+    className={`tab-${tabName} ${openedTab === tabTitle ? "active-tab" : ""}`}
+    onClick={() => handleTabClick(tabTitle)}
+  >
+    <div className="tab-content">{tabTitle}</div>
+  </div>
+);
+
 const HomeTabs: FC = () => {
-  const [openedTab, setOpenedTab] = useState<string | null>("New & Trending");
+  const [openedTab, setOpenedTab] = useState<string | number>("New & Trending");
   const [hoveredTabIndex, setHoveredTabIndex] = useState<number | null>(null);
 
   const handleTabClick = (tab: string) => {
@@ -126,66 +179,40 @@ const HomeTabs: FC = () => {
           <div className="tabs-row">
             <div className="tabs-mini-slider">
               <div className="tabs-mini-slider-content">
-                <div
-                  className={`tab-newreleases ${
-                    openedTab === "New & Trending" ? "active-tab" : ""
-                  }`}
-                  onClick={() => handleTabClick("New & Trending")}
-                >
-                  <div className="tab-content">New & Trending</div>
-                </div>
-                <div
-                  className={`tab-topsellers ${
-                    openedTab === "Top Sellers" ? "active-tab" : ""
-                  }`}
-                  onClick={() => handleTabClick("Top Sellers")}
-                >
-                  <div className="tab-content">Top Sellers</div>
-                </div>
-                <div
-                  className={`tab-upcoming ${
-                    openedTab === "Popular Upcoming" ? "active-tab" : ""
-                  }`}
-                  onClick={() => handleTabClick("Popular Upcoming")}
-                >
-                  <div className="tab-content">Popular Upcoming</div>
-                </div>
-                <div
-                  className={`tab-specials ${
-                    openedTab === "Specials" ? "active-tab" : ""
-                  }`}
-                  onClick={() => handleTabClick("Specials")}
-                >
-                  <div className="tab-content">Specials</div>
+                <div className="tabs-row">
+                  {[
+                    { tabName: "newreleases", tabTitle: "New & Trending" },
+                    { tabName: "topsellers", tabTitle: "Top Sellers" },
+                    { tabName: "upcoming", tabTitle: "Popular Upcoming" },
+                    { tabName: "specials", tabTitle: "Specials" },
+                  ].map((tab, index) => (
+                    <Tab
+                      key={index}
+                      tabName={tab.tabName}
+                      tabTitle={tab.tabTitle}
+                      handleTabClick={handleTabClick}
+                      openedTab={openedTab}
+                    />
+                  ))}
                 </div>
               </div>
             </div>
           </div>
           <div className="tabs-content">
-            <TabContent
-              items={newAndTrending}
-              title="New & Trending"
-              isOpened={openedTab === "New & Trending"}
-              onTabHover={handleTabHover}
-            />
-            <TabContent
-              items={topSellers}
-              title="Top Sellers"
-              isOpened={openedTab === "Top Sellers"}
-              onTabHover={handleTabHover}
-            />
-            <TabContent
-              items={popularUpcoming}
-              title="Popular Upcoming"
-              isOpened={openedTab === "Popular Upcoming"}
-              onTabHover={handleTabHover}
-            />
-            <TabContent
-              items={specials}
-              title="Specials"
-              isOpened={openedTab === "Specials"}
-              onTabHover={handleTabHover}
-            />
+            {[
+              { items: newAndTrending, title: "New & Trending" },
+              { items: topSellers, title: "Top Sellers" },
+              { items: popularUpcoming, title: "Popular Upcoming" },
+              { items: specials, title: "Specials" },
+            ].map((tab, index) => (
+              <TabContent
+                key={index}
+                items={tab.items}
+                title={tab.title}
+                isOpened={openedTab === tab.title}
+                onTabHover={handleTabHover}
+              />
+            ))}
           </div>
         </div>
         <div className="tab-right">
@@ -205,155 +232,39 @@ const HomeTabs: FC = () => {
                 </h2>
                 <div className="tab-review-summary">
                   <div className="review-title">Overall user reviews:</div>
-                  <span className="game-review-summary positive"></span>
+                  <span className="game-review-summary positive">Positive</span>
                   <span>&nbsp;(3,549)</span>
                 </div>
-                <div className="tags">
-                  {openedTab === "New & Trending" &&
-                    newAndTrending[hoveredTabIndex].tag1 && (
-                      <a href="">{newAndTrending[hoveredTabIndex].tag1}</a>
-                    )}
-                  {openedTab === "Top Sellers" &&
-                    topSellers[hoveredTabIndex].tag1 && (
-                      <a href="">{topSellers[hoveredTabIndex].tag1}</a>
-                    )}
-                  {openedTab === "Popular Upcoming" &&
-                    popularUpcoming[hoveredTabIndex].tag1 && (
-                      <a href="">{popularUpcoming[hoveredTabIndex].tag1}</a>
-                    )}
-                  {openedTab === "Specials" &&
-                    specials[hoveredTabIndex].tag1 && (
-                      <a href="">{specials[hoveredTabIndex].tag1}</a>
-                    )}
-                  {openedTab === "New & Trending" &&
-                    newAndTrending[hoveredTabIndex].tag2 && (
-                      <a href="">{newAndTrending[hoveredTabIndex].tag2}</a>
-                    )}
-                  {openedTab === "Top Sellers" &&
-                    topSellers[hoveredTabIndex].tag2 && (
-                      <a href="">{topSellers[hoveredTabIndex].tag2}</a>
-                    )}
-                  {openedTab === "Popular Upcoming" &&
-                    popularUpcoming[hoveredTabIndex].tag2 && (
-                      <a href="">{popularUpcoming[hoveredTabIndex].tag2}</a>
-                    )}
-                  {openedTab === "Specials" &&
-                    specials[hoveredTabIndex].tag2 && (
-                      <a href="">{specials[hoveredTabIndex].tag2}</a>
-                    )}
-                  {openedTab === "New & Trending" &&
-                    newAndTrending[hoveredTabIndex].tag3 && (
-                      <a href="">{newAndTrending[hoveredTabIndex].tag3}</a>
-                    )}
-                  {openedTab === "Top Sellers" &&
-                    topSellers[hoveredTabIndex].tag3 && (
-                      <a href="">{topSellers[hoveredTabIndex].tag3}</a>
-                    )}
-                  {openedTab === "Popular Upcoming" &&
-                    popularUpcoming[hoveredTabIndex].tag3 && (
-                      <a href="">{popularUpcoming[hoveredTabIndex].tag3}</a>
-                    )}
-                  {openedTab === "Specials" &&
-                    specials[hoveredTabIndex].tag3 && (
-                      <a href="">{specials[hoveredTabIndex].tag3}</a>
-                    )}
-                  {openedTab === "New & Trending" &&
-                    newAndTrending[hoveredTabIndex].tag4 && (
-                      <a href="">{newAndTrending[hoveredTabIndex].tag4}</a>
-                    )}
-                  {openedTab === "Top Sellers" &&
-                    topSellers[hoveredTabIndex].tag4 && (
-                      <a href="">{topSellers[hoveredTabIndex].tag4}</a>
-                    )}
-                  {openedTab === "Popular Upcoming" &&
-                    popularUpcoming[hoveredTabIndex].tag4 && (
-                      <a href="">{popularUpcoming[hoveredTabIndex].tag4}</a>
-                    )}
-                  {openedTab === "Specials" &&
-                    specials[hoveredTabIndex].tag4 && (
-                      <a href="">{specials[hoveredTabIndex].tag4}</a>
-                    )}
-                  {openedTab === "New & Trending" &&
-                    newAndTrending[hoveredTabIndex].tag5 && (
-                      <a href="">{newAndTrending[hoveredTabIndex].tag5}</a>
-                    )}
-                  {openedTab === "Top Sellers" &&
-                    topSellers[hoveredTabIndex].tag5 && (
-                      <a href="">{topSellers[hoveredTabIndex].tag5}</a>
-                    )}
-                  {openedTab === "Popular Upcoming" &&
-                    popularUpcoming[hoveredTabIndex].tag5 && (
-                      <a href="">{popularUpcoming[hoveredTabIndex].tag5}</a>
-                    )}
-                  {openedTab === "Specials" &&
-                    specials[hoveredTabIndex].tag5 && (
-                      <a href="">{specials[hoveredTabIndex].tag5}</a>
-                    )}
+                <div className="tab-tags">
+                  {openedTab === "New & Trending" ||
+                  openedTab === "Top Sellers" ||
+                  openedTab === "Popular Upcoming" ||
+                  openedTab === "Specials"
+                    ? [...Array(5).keys()].map((index) => {
+                        const tag = (
+                          openedTab === "New & Trending"
+                            ? newAndTrending[hoveredTabIndex]
+                            : openedTab === "Top Sellers"
+                            ? topSellers[hoveredTabIndex]
+                            : openedTab === "Popular Upcoming"
+                            ? popularUpcoming[hoveredTabIndex]
+                            : specials[hoveredTabIndex]
+                        ) as TabItem;
+
+                        const tagText = tag[`tag${index + 1}` as keyof TabItem];
+                        return (
+                          tagText && (
+                            <a key={index} href="">
+                              {tagText}
+                            </a>
+                          )
+                        );
+                      })
+                    : null}
                 </div>
-                <div
-                  className="tabs-screenshot"
-                  style={{
-                    backgroundImage: `url(${
-                      openedTab === "New & Trending"
-                        ? newAndTrending[hoveredTabIndex].image1
-                        : openedTab === "Top Sellers"
-                        ? topSellers[hoveredTabIndex].image1
-                        : openedTab === "Popular Upcoming"
-                        ? popularUpcoming[hoveredTabIndex].image1
-                        : openedTab === "Specials"
-                        ? specials[hoveredTabIndex].image1
-                        : ""
-                    })`,
-                  }}
-                ></div>
-                <div
-                  className="tabs-screenshot"
-                  style={{
-                    backgroundImage: `url(${
-                      openedTab === "New & Trending"
-                        ? newAndTrending[hoveredTabIndex].image2
-                        : openedTab === "Top Sellers"
-                        ? topSellers[hoveredTabIndex].image2
-                        : openedTab === "Popular Upcoming"
-                        ? popularUpcoming[hoveredTabIndex].image2
-                        : openedTab === "Specials"
-                        ? specials[hoveredTabIndex].image2
-                        : ""
-                    })`,
-                  }}
-                ></div>
-                <div
-                  className="tabs-screenshot"
-                  style={{
-                    backgroundImage: `url(${
-                      openedTab === "New & Trending"
-                        ? newAndTrending[hoveredTabIndex].image3
-                        : openedTab === "Top Sellers"
-                        ? topSellers[hoveredTabIndex].image3
-                        : openedTab === "Popular Upcoming"
-                        ? popularUpcoming[hoveredTabIndex].image3
-                        : openedTab === "Specials"
-                        ? specials[hoveredTabIndex].image3
-                        : ""
-                    })`,
-                  }}
-                ></div>
-                <div
-                  className="tabs-screenshot"
-                  style={{
-                    backgroundImage: `url(${
-                      openedTab === "New & Trending"
-                        ? newAndTrending[hoveredTabIndex].image4
-                        : openedTab === "Top Sellers"
-                        ? topSellers[hoveredTabIndex].image4
-                        : openedTab === "Popular Upcoming"
-                        ? popularUpcoming[hoveredTabIndex].image4
-                        : openedTab === "Specials"
-                        ? specials[hoveredTabIndex].image4
-                        : ""
-                    })`,
-                  }}
-                ></div>
+                <div className="tabs-screenshots">
+                  {renderScreenshots(openedTab, hoveredTabIndex)}
+                </div>
               </div>
             )}
           </div>
